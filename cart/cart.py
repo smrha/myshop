2abe4bccfe6caeb4e7162b7f9693efdff9b429a0
smrha <lord.smrha@gmail.com>
2023-11-04 15:35:38 +0330

    Initialize the cart app.

---
+++ cart/cart.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from django.conf import settings
+from shop.models import Product
+
+class Cart:
+    """
+    Initialize the cart.
+    """
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            # save an empty cart in the session
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
